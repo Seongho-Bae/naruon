beb0d91ae081fb6d4a3509bd0f8127485acfe495
Seongho Bae <me@seonghobae.me>
2026-06-30 09:03:40 +0900

    Remove stray XSS smoke artifact

--- test_xss.js
+++
@@ -1,6 +0,0 @@
-const { JSDOM } = require('jsdom');
-const window = new JSDOM('').window;
-global.window = window;
-global.document = window.document;
-const DOMPurify = require('dompurify');
-console.log(DOMPurify.sanitize('<script>문서 원본 검토</script>', { ALLOWED_TAGS: [] }).trim() || '제목 없는 작업');
